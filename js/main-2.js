93bc1d9fb1408366eda01696c19003d7808c2444
elenyou <elenyou333@gmail.com>
2019-03-13 16:40:54 -0400

    addad labels and axis

--- js/main-2.js
+++ js/main-2.js
@@ -1,31 +1,77 @@
-const svg2 = d3
+const margin = { left: 100, right: 10, top: 10, bottom: 100 };
+
+const width = 600 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
+
+const g = d3
   .select("#chart-area-2")
   .append("svg")
-  .attr("width", 600)
-  .attr("height", 400);
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+g.append("text")
+  .attr("class", "x axis-label")
+  .attr("x", width / 2)
+  .attr("y", height + 80)
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .text("The price range for auto");
+
+g.append("text")
+  .attr("class", "y axis-label")
+  .attr("x", - (height / 2))
+  .attr("y", -60)
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .attr("transform", "rotate(-90)")
+  .text("Price ($)");
 
 d3.json("data/pricelist.json")
   .then(function(data) {
     data.map(d => (d.price = +d.price));
 
-    const y = d3.scaleLinear()
-      .domain([0, 1250000])
-      .range([0, 400]);
+    const x = d3
+      .scaleBand()
+      .domain(data.map(d => d.name))
+      .range([0, width])
+      .paddingInner(0.3)
+      .paddingOuter(0.3);
 
-    const color = d3.scaleOrdinal()
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, d => d.price)])
+      .range([0, height]);
+
+    const color = d3
+      .scaleOrdinal()
       .domain(data)
       .range(["RED", "BLUE", "ORANGE", "GREEN", "INDIGO", "GREY"]);
 
+    const xAxisCall = d3.axisBottom(x);
+    g.append("g")
+      .attr("class", "x-axis")
+      .attr("transofm", "translate(0, " + height + ")")
+      .call(xAxisCall);
 
-    svg2
-      .selectAll("rect")
+    const yAxisCall = d3.axisLeft(y)
+      .ticks(10)
+      .tickFormat(function(d) {
+        return d + "$";
+      });
+    g.append("g")
+      .attr("class", "y-axis")
+      .call(yAxisCall);
+
+    g.selectAll("rect")
       .data(data)
       .enter()
       .append("rect")
-      .attr("x", (d, i) => (i*80))
+      .attr("x", d => x(d.name))
       .attr("y", 20)
-      .attr("width", 60)
-      .attr("height", d => y(d.price *20))
+      .attr("width", x.bandwidth)
+      .attr("height", d => y(d.price))
       .style("fill", (d, i) => color(d.name));
   })
   .catch(e => {
